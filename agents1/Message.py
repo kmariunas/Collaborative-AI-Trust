1aa33d05a3948d66a38d8f931adff887ab932966
ahautelman <P.A.Hautelman-1@student.tudelft.nl>
2022-03-20 15:10:09 +0100

    add comments and change the create_message method to use objects instead of strings

--- agents1/Message.py
+++ agents1/Message.py
@@ -48,11 +48,34 @@ def extract_room(content):
 
 
 class MessageBuilder:
+    """
+    Class for sending and reading messages following the communication protocol mentioned in the assignment. To
+    create new messages, make sure to add a new MessageType, and implement the create_message and process_message
+    methods.
+
+    Implemented custom messages:
+    * FOUND_BLOCK: "Found block [block_vis] at location [location]"
+    """
 
     def __init__(self, agent_name):
         self.agent_name = agent_name
 
     def create_message(self, mt, room_name=None, block_vis=None, location=None):
+        """
+        Method returns a matrx Message object with a string content built with the passed parameters
+
+        @param mt: MessageType
+        @param room_name: str, thr room name (ie: room_3)
+        @param block_vis: a dictionary as the one used in the BW4TBaselineAgent to store the block visualization.
+            Dict must contain 'size', 'colour', and 'shape' keys and values
+        @param location: list with coordinates (ie: [8, 8])
+
+        @return: matrx Message.
+        """
+
+        block_vis = MessageBuilder._block_vis_str(block_vis)
+        location = MessageBuilder._location_str(location)
+
         # STANDARD MESSAGES
         if mt is MessageType.OPEN_DOOR:
             msg = "Opening door of " + room_name
@@ -77,7 +100,7 @@ class MessageBuilder:
         return Message(content=msg, from_id=self.agent_name)
 
     @staticmethod
-    def block_vis_str(block_vis):
+    def _block_vis_str(block_vis):
         """
         Method takes in block visualization and returns the correct string representation.
         """
@@ -89,7 +112,7 @@ class MessageBuilder:
         return res
 
     @staticmethod
-    def location_str(location):
+    def _location_str(location):
         """
         Method takes in location and returns the correct string representation.
         """
@@ -99,7 +122,8 @@ class MessageBuilder:
     @staticmethod
     def process_message(msg):
         """
-        Method takes in matrx Message and parses the content into a dictionary:
+        Method takes in matrx Message and parses the content into a dictionary
+        @param msg: matrx Message
         @return dict: {
                         'from_id': id the sender agent,
                         'type': MessageType,
