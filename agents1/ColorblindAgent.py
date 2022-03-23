f44a3dabc46699bda47f797eae6748d036fb0d87
Karolis Mariunas <kmariunass@gmail.com>
2022-03-23 14:32:03 +0100

    Update checking surroundings
    sends message FOUND_BLOCK instead of FOUND_GOAL_BLOCK whenever a similar block is found

--- agents1/ColorblindAgent.py
+++ agents1/ColorblindAgent.py
@@ -2,6 +2,7 @@
 from typing import Dict
 from agents1.GenericAgent import GenericAgent
 from agents1.Phase import Phase
+from agents1.Message import MessageType
 
 class ColorblindAgent(GenericAgent):
 
@@ -21,3 +22,19 @@ class ColorblindAgent(GenericAgent):
                 block['visualization']['colour'] = None
 
         return state
+
+    def check_surroundings_for_box(self, state):
+        blocks = [(block['visualization'], block['location'], block['obj_id']) for block in state.values() if
+                  'class_inheritance' in block and 'CollectableBlock' in block['class_inheritance']]
+
+        # check if any of the found blocks are our goal block
+        for block, location, obj_id in blocks:
+            for key, goal_block in self._goal_blocks.items():
+
+                if block['shape'] == goal_block['visualization']['shape'] \
+                        and block['size'] == goal_block['visualization']['size']:
+
+                    msg = self._mb.create_message(MessageType.FOUND_BLOCK,
+                                                  block_vis=block,
+                                                  location=location)
+                    self._sendMessage(msg)
