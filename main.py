dbe4c3cfe41adfe2384c51db3c7939f154c6a174
Benedict <A.B.C.Bien@student.tudelft.nl>
2022-03-26 03:59:54 +0100

    Forgot to commit this file as well

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from agents1.BW4TBaselineAgent import BaseLineAgent
 from agents1.BW4THuman import Human
 from agents1.GenericAgent import GenericAgent
+from agents1.GenericAgentTesting import GenericAgentTesting
 from agents1.LazyAgent import LazyAgent
+from agents1.StrongAgent import StrongAgent
 from bw4t.BW4TWorld import BW4TWorld
 from bw4t.statistics import Statistics
 
@@ -12,7 +14,7 @@ press the start button in god mode to start the session.
 
 if __name__ == "__main__":
     agents = [
-        {'name':'agent1', 'botclass':LazyAgent, 'settings':{}}, # 'slowdown':10
+        {'name':'agent1', 'botclass':GenericAgentTesting, 'settings':{}}, # 'slowdown':10
         # {'name':'agent2', 'botclass':LazyAgent, 'settings':{}},
         # {'name': 'agent3', 'botclass': LazyAgent, 'settings': {}},
         # {'name':'human', 'botclass':Human, 'settings':{}}
